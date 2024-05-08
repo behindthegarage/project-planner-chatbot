878224d0822f668306db167f7611e5ecc3492c02
behindthegarage <behindthegarage.dev>
2024-05-08 18:58:32 -0400

    Commit unsaved changes of previous commit re archive folder. Added browse_emails function.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
 from dotenv import load_dotenv
 from process_emails import main as process_emails_main
+from browse_emails import main as browse_emails_main
 
 # Load environment variables
 load_dotenv()
 
 if __name__ == "__main__":
     process_emails_main()
+    browse_emails_main()
